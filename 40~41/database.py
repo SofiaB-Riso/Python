c698f6c0f221047f88b9fe2ebe45e83139d28fb0
SofiaB-Riso <sbriso2020@gmail.com>
2026-07-10 18:24:11 -0300

    Update database.py

--- 40~41/database.py
+++ 40~41/database.py
@@ -1,1 +1,28 @@
+import sqlite3
+
+conn = sqlite3.connect("tarefas.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    senha TEXT NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS tarefas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    descricao TEXT,
+    status TEXT NOT NULL,
+    usuario_id INTEGER,
+    FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+)
+""")
+
+conn.commit()
+conn.close()
 
